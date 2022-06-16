48868d5d73b3d56d6149309f859f82d67a623d1b
brenimdelas <brenolegallouco@gmail.com>
2022-06-15 21:53:30 -0300

    adicionando-evento-no-menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-function ocultarexibirmenu () {
-    
-    alert ("ola mundo");
+function ocultarexibirmenu (nome) {    
+    var menu = document.getElementById ("menu-header");
+    var icone = document.getElementById ("icone-menu");
+    if (getComputedStyle(menu).display =='none  ') {
+        menu.style.display ="flex";
+        icone.classList.remove ("fa-bars");
+        icone.classList.add("fa-times");
+    } else {
+        menu.style.display ="none";
+        icone.classList.remove ("fa-times");
+        icone.classList.add("fa-bars");
+    }
+
+}   
 
 
-}
