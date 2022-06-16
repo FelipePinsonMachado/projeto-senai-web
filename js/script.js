2909c4f84d8ab0988fb604df9aea7e913b93368e
unknown <viniciusramositu@gmail.com>
2022-06-15 21:52:09 -0300

    adicionando evento no menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-function ocultarmostrarmenu () {
+function ocultarmostrarmenu (nome) {
     
-     alert ("ola mundo");
-
-} 
+     
+     var menu  = document.getElementById ("menu");
+     var icone = document.getElementById ("icone-menu");
+      if (getComputedStyle(menu).display == 'none' ) {
+         menu.style.display = "flex";  
+         icone.classList.remove("fa-bars");
+         icone.classList.add("fa-times");     
+       } else {
+          menu.style.display = "none";
+          icone.classList.remove("fa-times");
+          icone.classList.add("fa-bars");
+        }
+    }
