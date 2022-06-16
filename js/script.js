88bf97f8b71bbfae0c588dc229730a77f3611c4d
tiago <tiagofeliperossiribeiro505@gmail.com>
2022-06-15 21:53:39 -0300

    adicionando evento no menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-function ocultarmostrarmenu () {
-    alert("ola mundo");
+function ocultarmostrarmenu (nome) {
+    var menu   =document.getElementById("menu-header");
+    var icone  =document.getElementById("icone-menu");
+    if (getComputedStyle (menu).display == 'none') {
+        menu.style.display = "flex";
+        icone.classList.remove ("fa-bars");
+        icone.classList.add ("fa-times");
+    }else {
+        menu.style.display = "none";
+        icone.classList.remove ("fa-times");
+        icone.classList.add ("fa-bars");
+    }
 }
