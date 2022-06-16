e21b9f8dd80761d67f3bb09faf067d4258984c5f
Kevin-auto-escola <kevinmatthewsrs@hotmail.com>
2022-06-15 21:54:00 -0300

    adicionando evento no menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-function OcultarMostrarMenu (){
-    alert("ola mundo");
+function MostrarMenu (nome){
+    var menu =  document.getElementById("menu-de-navegacao");
+    var icone = document.getElementById("icone-menu");
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = "flex";
+        icone.classList.remove("fa-bars");
+        icone.classList.add("fa-times");
+    } else {
+        menu.style.display = "none";
+        icone.classList.remove("fa-times");
+        icone.classList.add("fa-bars");
+    }
+    
 
 }
