ab724848fd2a2b656737b0b9e8f023b5f3163c41
higusokfofo <higorlucenahh@gmail.com>
2022-06-15 21:52:20 -0300

    adicionandoeventonomenu

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,18 @@
-function ocultarexibirmenu () {
+function ocultarexibirmenu (nome) {
 
-    alert ("Olá mundo");
+    var menu = document.getElementById("menu-header");
+    var icone = document.getElementById("icone-menu");
 
+if (getComputedStyle(menu).display == 'none') {
+
+        menu.style.display = "flex";
+        icone.classList.remove("fa-align-justify");
+        icone.classList.add("fa-times");
+
+  } else {
+
+        menu.style.display = "none";
+        icone.classList.remove("fa-times");
+        icone.classList.add("fa-align-justify");
+ }
 }
