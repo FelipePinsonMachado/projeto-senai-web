df6b067f2d05af6f0f19b7e748c98da3d3aff392
vittor.xyz <vitinhohugorf14@gmail.com>
2022-06-20 20:10:28 -0300

    treinando funçoes

---
+++ calculadora/_js/script.js
@@ -0,0 +1,18 @@
+function calcularNumero() {
+
+}
+function registrarNumero(acoes) {
+    //pegamos o valor digitado na tela
+var telaAtual = document.form.displayCalc.value;
+    //escrever na tela o valor do atual (telaAtual) + o novo valor (acoes)
+document.form.displayCalc.value = telaAtual + acoes; 
+
+
+}
+
+function clear() {
+
+}
+ function allclear() {
+
+ }
