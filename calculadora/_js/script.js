a4866b8e972c03cfb998a472c60b6b4c0cbc8177
vittor.xyz <vitinhohugorf14@gmail.com>
2022-06-20 21:26:23 -0300

    treinando funcoes parte 2

--- calculadora/_js/script.js
+++ calculadora/_js/script.js
@@ -1,18 +1,29 @@
-function calcularNumero() {
-
+function calcularNumero() { 
+    //se usa o eval para haver uma soma entre o tela atual e o
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = eval (telaAtual)
 }
+
+
 function registrarNumero(acoes) {
     //pegamos o valor digitado na tela
 var telaAtual = document.form.displayCalc.value;
     //escrever na tela o valor do atual (telaAtual) + o novo valor (acoes)
 document.form.displayCalc.value = telaAtual + acoes; 
-
-
+    
 }
 
-function clear() {
+function apagar() {
+    //apagar os numeros de 1 em 1 de tras pra frente
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = telaAtual.substring (0, telaAtual.length -1);
+
+   
+  
 
 }
  function allclear() {
+    // apagar toods os numeros
+    document.form.displayCalc.value = "";
 
  }
