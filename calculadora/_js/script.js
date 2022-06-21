1836ef04b94bad6eb8212aa886cca7f0af01f041
Yan <yanalessandro766@gmail.com>
2022-06-20 21:25:46 -0300

    treinandofuncoespt2

--- calculadora/_js/script.js
+++ calculadora/_js/script.js
@@ -1,26 +1,29 @@
 function calcularNumero (){
-
-
+//pegamos o valor atual digitado na tela
+var telaAtual = document.form.displayCalc.value;
+document.form.displayCalc.value = eval(telaAtual);
 
 }
 
 function registrarNumeros (acoes){
 
-
+//pegamos o valor atual digitado na tela
 var telaAtual = document.form.displayCalc.value;
+//escrever na tela o valor atual (telaAtual) juntando  com o novo valor (acoes)
 document.form.displayCalc.value = telaAtual + acoes;
 
 }
 
-function clear (){
+function apagar (){
 
-
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = telaAtual.substring(0, telaAtual.length-1);
 
 }
 
 function allClear (){
 
-
+    document.form.displayCalc.value = "" ;
 
 
 }
