a9161945b362d0f6a1c14d4fae130fac2befce9a
tiago <tiagofeliperossiribeiro505@gmail.com>
2022-06-20 21:26:26 -0300

    treinando funcoes parte 2

--- calculadora/_js/script.js
+++ calculadora/_js/script.js
@@ -1,12 +1,26 @@
-function calcularNumero () {}
+function calcularNumero () 
+{
+    //para calcular o resultado da minha conta 
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = eval(telaAtual);
+}
+
 function registrarNumeros (acoes) 
 {
-    /* pegamos o valor atual digitando na tela */
+    // pegamos o valor atual digitando na tela 
      var telaAtual = document.form.displayCalc.value;
-     /*escrever na tela o valor atual (telaAtual) + o novo valor (acoes) */
+     //escrever na tela o valor atual (telaAtual) + o novo valor (acoes)
      document.form.displayCalc.value = telaAtual + acoes;    
 }
-function clear () {
-
+function apagar () 
+{
+    // para apagar
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = telaAtual.substring (0, telaAtual.length -1);
 }
-function allclear () {}
+
+function allclear () 
+{
+    //para apagar tudo 
+    document.form.displayCalc.value = "";
+}
