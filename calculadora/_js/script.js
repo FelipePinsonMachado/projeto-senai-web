affbf8c902976774f05c5e037b8183a53221a4cb
higusokfofo <higorlucenahh@gmail.com>
2022-06-20 21:24:27 -0300

    usandofuncoesPART2

--- calculadora/_js/script.js
+++ calculadora/_js/script.js
@@ -1,16 +1,23 @@
-function calcularNumero () {
+function calcularNumero () { 
+
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = eval(telaAtual);
 
 }
 function registrarNumeros (acoes) {
 
+    //PEGAMOS O VALOR ATUAL DIGITADO NA TELA
     var telaAtual = document.form.displayCalc.value;
-
+    //ESCREVER NA TELA O VALOR ATUAL (telaAtual) JUTANTO COM O NOVO VALOR (ACOES)
     document.form.displayCalc.value = telaAtual + acoes;
 
 }
-function clear () {
+function apagar () {
+
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = telaAtual.substring(0, telaAtual.length -1)
 
 }
 function allClear () {
-
+    document.form.displayCalc.value = "";
 }
