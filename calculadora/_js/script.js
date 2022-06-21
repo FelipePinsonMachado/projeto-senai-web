5bd703fdcfcd6ac5cbd17010a622d4d62625296c
brenimdelas <brenolegallouco@gmail.com>
2022-06-20 21:24:29 -0300

    treinando-funcoes-parte-2

--- calculadora/_js/script.js
+++ calculadora/_js/script.js
@@ -1,18 +1,23 @@
 function calcularNumero () {
-
+    //calcular o numero, puxado do registrar numero
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = eval(telaAtual);
 }
 function registrarNumero (action) {
+    //pegamos o valor digitado na tela
     var telaAtual = document.form.displayCalc.value;
-    document.form.displayCalc.value= telaAtual + action;
-    
-
-
-
+    document.form.displayCalc.value= telaAtual + action; 
 }
-function clear () {
+function apagar () {
+    //apagar os numeros de 1 em 1 de tras pra frente
+    var telaAtual = document.form.displayCalc.value;
+    document.form.displayCalc.value = telaAtual.substring (0,telaAtual.length -1);
+
 
 }
 
 function allClear () {
+    //apagar todos os numeros
+    document.form.displayCalc.value = "";
 
 }
