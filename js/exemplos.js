c43f4fdc59e3d33527930aae605d69a4e4fc42ea
Kevin-auto-escola <kevinmatthewsrs@hotmail.com>
2022-06-22 21:48:14 -0300

    aprendendo repetição

--- js/exemplos.js
+++ js/exemplos.js
@@ -1,1 +1,9 @@
-alert ("assss");
+
+function testeFor (){
+    let cont;
+    for(cont=0; cont <= 10; cont++)
+    {
+        var mostrarContagem = document.getElementById("mostrar-contagem");
+        mostrarContagem.innerHTML += `Numero` + cont + "<br>";
+    }
+}                                                                                                                                                                                                 
