2e7a7201265e87fa9147305f8685765eeb33c834
Yan <yanalessandro766@gmail.com>
2022-06-22 21:51:46 -0300

    estruturasderepeticao

--- js/exemplos.js
+++ js/exemplos.js
@@ -1,1 +1,10 @@
-alert ('teste');
+
+function testeFor () {
+
+let cont;
+for(cont = 10;cont >= 0; cont--){
+
+    var mostrarContagem = document.getElementById ("mostrar-contagem");
+    mostrarContagem.innerHTML += "Numero " + cont + "<br>";
+}
+} 
