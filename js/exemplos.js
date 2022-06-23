6f96c96e6ecc4e12f926e076e79d4620b100f9bc
unknown <viniciusramositu@gmail.com>
2022-06-22 21:50:35 -0300

    aprendendo repetiçao

--- js/exemplos.js
+++ js/exemplos.js
@@ -1,1 +1,15 @@
-alert (teste);
+
+function testeFor (){
+      
+    let cont;
+    for (cont = 10; cont >=0;cont--)
+    {
+        var mostrarcontagem = document.getElementById("mostrar-contagem");
+        mostrarcontagem.innerHTML += "numero" + cont + "<br>";       
+    }
+    
+    
+}
+
+
+
