d33de63cdc4a62dc292fa0760b464d5ae0c06e75
higusokfofo <higorlucenahh@gmail.com>
2022-06-22 21:50:03 -0300

    estruturaderepeticao

--- exemplos/exeplos.js
+++ exemplos/exeplos.js
@@ -1,1 +1,8 @@
-alert ('tests')
+function testeFor () { 
+    let cont; 
+    for (cont = 10; cont >= 1; cont--) {
+        var mostrarContagem = document.getElementById("mostrarContagem");
+        mostrarContagem.innerHTML += "Numero " + cont + "<br>";
+
+    }
+}
