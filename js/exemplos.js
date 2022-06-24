f998c97bc1562511a24298b849159aa527397ecb
Kevin-auto-escola <kevinmatthewsrs@hotmail.com>
2022-06-24 20:05:42 -0300

    aprendendo repetição parte dois

--- js/exemplos.js
+++ js/exemplos.js
@@ -3,7 +3,19 @@ function testeFor (){
     let cont;
     for(cont=0; cont <= 10; cont++)
     {
-        var mostrarContagem = document.getElementById("mostrar-contagem");
+        var mostrarContagem = document.getElementById("mostrar-contagem-for");
         mostrarContagem.innerHTML += `Numero` + cont + "<br>";
+
     }
-}                                                                                                                                                                                                 
+
+}   
+
+function testewhile (){
+    var cont = 11;
+    while(cont > 0)
+    {
+        var mostrarNumeros = document.getElementById("mostrar-contagem-while");
+        mostrarNumeros.innerHTML += `Numero` + cont + "<br>"
+        cont--;    
+    }
+}
