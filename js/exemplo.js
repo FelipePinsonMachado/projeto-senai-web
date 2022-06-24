aee967f9ec8eea0d4d53409ec70de70e94732396
brenimdelas <brenolegallouco@gmial.com>
2022-06-24 19:24:14 -0300

    b

--- js/exemplo.js
+++ js/exemplo.js
@@ -22,3 +22,11 @@ var comparacao=(20<10);
     var tester=comparacao ? alert (true) :alert(false);
         
 
+    function testeFor() {
+        let cont;
+        for(cont=10; cont >0; cont--){
+         var mostrarContagem = document.getElementById("MostrarContagem");
+         mostrarContagem.innerHTML += "Numero" + cont + "<br>";    
+        }  
+    } 
+    
