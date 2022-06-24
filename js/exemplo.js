085034e3195cd65170c928498bc901be1fe39b8f
brenimdelas <brenolegallouco@gmial.com>
2022-06-24 19:22:53 -0300

    a

--- js/exemplo.js
+++ js/exemplo.js
@@ -1,1 +1,24 @@
-alert ("amfjafj");
+
+
+function testeFor() {
+    let cont;
+    for(cont=10; cont >0; cont--){
+     var mostrarContagem = document.getElementById("MostrarContagem");
+     mostrarContagem.innerHTML += "Numero" + cont + "<br>";    
+    }  
+} 
+
+function somar (){
+
+var numero1=document.numeros.numeroinicial
+var numero2=document.numeros.numerofinal
+
+
+
+    
+}
+var comparacao=(20<10);
+
+    var tester=comparacao ? alert (true) :alert(false);
+        
+
