205b89bac1b1be27148cbfe60c9024a5a15dcef1
brenimdelas <brenolegallouco@gmial.com>
2022-06-24 21:11:43 -0300

    aprendendo-repeticao-parte-3

--- js/exemplo.js
+++ js/exemplo.js
@@ -7,7 +7,7 @@ function testeFor() {
      mostrarContagem.innerHTML += "Numero" + cont + "<br>";    
     }  
 } 
-
+/** 
 function somar (){
 
 var numero1=document.numeros.numeroinicial
@@ -16,14 +16,7 @@ var numero2=document.numeros.numerofinal
 
 
     
-}     
-    function somar() {
-        let cont;
-        for(cont=10; cont >0; cont--){
-         var mostrarContagem = document.getElementById("MostrarContagem");
-         mostrarContagem.innerHTML += "Numero" + cont + "<br>";    
-        }  
-    } 
+}**/
  function testWhile() {
      let cont=20;
      while(cont>5) {
@@ -32,4 +25,19 @@ var numero2=document.numeros.numerofinal
      }
 
  }
+ function mostrarSwitch() {
+     let data_hora = new Date();
+     let dia_semana=data_hora.getDay();     
+     switch(dia_semana)
+     {
+        case 0:
+             alert("Domingo");
+            break;
+        case 5 :alert("sexta-feira");      
+            break;   
+        case 6: alert("sabado");
+            break;
+        default: alert("semana longa");
+}
+}
     
