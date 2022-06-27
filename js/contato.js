635dc93f1dae9c4dacfc792d2ecff9310586175c
Yan <yanalessandro766@gmail.com>
2022-06-27 20:32:29 -0300

    lendoinformçaoesdefomulariocomfunçao

---
+++ js/contato.js
@@ -0,0 +1,18 @@
+
+ function enviarFormulario()
+ {
+     
+    var nome = document.formContato.nome.value;
+    alert (nome);
+
+    var fone = document.formContato.fone.value;
+    alert (fone);
+
+    var mensagem = document.formContato.mensagem.value;
+    alert (mensagem);
+
+    var assunto = document.formContato.assunto.value;
+    alert (assunto);
+ }
+
+ 
