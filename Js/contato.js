bd1f86d38d8fc2b6e230e0ec400b2348424da8e3
higusokfofo <higorlucenahh@gmail.com>
2022-06-27 20:30:57 -0300

    lendoinformaçaodeformulariocomfunçao

---
+++ Js/contato.js
@@ -0,0 +1,13 @@
+
+ function enviarFormulario(){
+
+    var nome = document.formContato.nome.value;
+    var fone = document.formContato.fone.value;
+    var assunto = document.formContato.assunto.value;
+    var mensagem = document.formContato.mensagem.value;
+
+    alert (nome);
+    alert (fone);
+    alert (assunto);
+    alert (mensagem);
+ }
