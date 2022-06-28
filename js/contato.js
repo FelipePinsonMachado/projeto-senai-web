19a80e25370a6a38660542699e2baaaae4852dce
vittor.xyz <vitinhohugorf14@gmail.com>
2022-06-27 21:42:29 -0300

    informações de formularuio com função?

--- js/contato.js
+++ js/contato.js
@@ -4,16 +4,41 @@
  * Exibe uma mensagem na tela para o cliente
  *
  */
- function enviarFormulario()
- {
+ function enviarFormulario(){
+ 
 
     var nomeCliente = document.formContato.nome.value;
     var telefoneCliente = document.formContato.fone.value;
     var assuntoCliente = document.formContato.assunto.value;
     var mensagemCliente = document.formContato.mensagem.value;
-    alert(nomeCliente);
-    alert(telefoneCliente);
-    alert(assuntoCliente);
-    alert(mensagemCliente);
 
+    
+    
+
+    if (nomeCliente == "" || assuntoCliente == "" || telefoneCliente == "" ||  mensagemCliente == "")  {
+      alert("O FORMULARIO PRECISA SER PREENCHIDO")
+   return;
+   }
+    
+     alert("FORMULARIO ENVIADO COM SUCESSO")
+    
+ 
+
+      document.formContato.nome.value="";
+      document.formContato.fone.value="";
+      document.formContato.assunto.value="";
+      document.formContato.mensagem.value="";
+  
+   
+     
+
+       
+        
+
+
+
+
+
+
+    
  }
