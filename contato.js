123562dce98bc65b72a52d218351eba2f217785e
joao <joaovictorfc21@gmail.com>
2022-06-27 21:48:23 -0300

    validaçoesnoformulariocomfunçao

--- contato.js
+++ contato.js
@@ -3,14 +3,19 @@
  {
  
   var nome = document.formContato.nome.value;
-  alert (nome);
-
   var fone = document.formContato.fone.value;
-  alert (fone);
-
   var assunto = document.formContato.assunto.value;
-  alert (assunto);
-
   var mensagem = document.formContato.mensagem.value;
-  alert (mensagem);
- }
+
+  if(nome =="" || fone =="" || assunto =="" || mensagem =="") {
+    alert ("campos nao podem ser vazios");
+    return;
+  }
+       alert ("Formulario enviado com sucesso!");
+
+       document.formContato.nome.value = "";
+   document.formContato.fone.value = "";
+   document.formContato.assunto.value = "";
+   document.formContato.mensagem.value = "";
+   }
+ 
