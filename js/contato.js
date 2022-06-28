261d24f6d73b5b33bc69a272e01ff25391159fb0
Kevin-auto-escola <kevinmatthewsrs@hotmail.com>
2022-06-27 21:43:40 -0300

    validação no formulário

--- js/contato.js
+++ js/contato.js
@@ -4,16 +4,28 @@
  * Exibe uma mensagem na tela para o cliente
  *
  */
+
  function enviarFormulario()
  {
      var nome = document.formContato.nome.value;
      var telefone = document.formContato.fone.value;
      var assunto = document.formContato.assunto.value;
      var mensagem = document.formContato.mensagem.value;
-     alert (nome);
-     alert (telefone);
-     alert (assunto);
-     alert (mensagem);
+        if (nome == ""){
+            alert ("nome não pode ser vazio");
+            return;
+        }
+        if (telefone == ""||assunto == ""||mensagem == ""){
+            alert ("formulario não pode ser vazio");
+            return;
+        }
+        alert ("formulario enviado com sucesso");
+        document.formContato.nome.value = "";
+        document.formContato.fone.value = "";
+        document.formContato.assunto.value = "";
+        document.formContato.mensagem.value ="";
+
+  
      
 
  }
