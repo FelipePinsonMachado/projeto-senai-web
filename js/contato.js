d6e29e1ed3308ccf911ba9f714611468144fe84a
Yan <yanalessandro766@gmail.com>
2022-06-27 21:48:31 -0300

    validacoesdeformulario

--- js/contato.js
+++ js/contato.js
@@ -3,16 +3,21 @@
  {
      
     var nome = document.formContato.nome.value;
-    alert (nome);
-
-    var fone = document.formContato.fone.value;
-    alert (fone);
-
-    var mensagem = document.formContato.mensagem.value;
-    alert (mensagem);
-
     var assunto = document.formContato.assunto.value;
-    alert (assunto);
+    var mensagem = document.formContato.mensagem.value;
+    var fone = document.formContato.fone.value;
+
+    if(nome =="" || assunto =="" || fone =="" || mensagem =="") {
+
+       alert ("campos nao podem ser vazios")
+        return;
+    }
+    alert ('Formulario enviado com sucesso!')
+
+    document.formContato.nome.value = "";
+    document.formContato.assunto.value = "";
+    document.formContato.mensagem.value = "";
+    document.formContato.fone.value = "";
  }
 
  
