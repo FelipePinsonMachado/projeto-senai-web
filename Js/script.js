95b6aa51b2638b4dabe0a6530a9e06685c4b801b
higusokfofo <higorlucenahh@gmail.com>
2022-07-04 21:43:48 -0300

    cartaoprolado

--- Js/script.js
+++ Js/script.js
@@ -14,5 +14,21 @@ if (getComputedStyle(menu).display == 'none') {
         menu.removeAttribute('style');
         icone.classList.remove("fa-times");
         icone.classList.add("fa-align-justify");
+        
  }
 }
+
+/**
+ * @param distMenu (posição que o menu ira aparecer na tela)
+ * @param distBody (posição de alinhamento das coisas com o corpo da pagina, o que ira fazer com que todo o conteudo ande junto com o menu.)
+ */
+
+ function exibirOcultarMenuLateral(distMenu, distBody) {
+    $('.menu').animate({
+        left: distMenu + "px"
+    }, 200);
+    $('body').animate({
+        left: distBody + "px"
+    }, 200);
+}
+
---
+++ MenuLateral/js.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param distMenu (posição que o menu ira aparecer na tela)
+ * @param distBody (posição de alinhamento das coisas com o corpo da pagina, o que ira fazer com que todo o conteudo ande junto com o menu.)
+ */
+
+ function exibirOcultarMenuLateral(distMenu, distBody) {
+    $('.menu').animate({
+        left: distMenu + "px"
+    }, 200);
+    $('body').animate({
+        left: distBody + "px"
+    }, 200);
+}
